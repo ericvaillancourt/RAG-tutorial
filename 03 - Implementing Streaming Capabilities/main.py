b4d7f1be19c4e73b470796218ea8e8744dd25918
Eric Vaillancourt <eric_vaillancourt@sympatico.ca>
2024-06-09 17:30:54 -0400

    Part3

--- 03 - Implementing Streaming Capabilities/main.py
+++ 03 - Implementing Streaming Capabilities/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_core.messages import AIMessageChunk
-#import logging
+#from logging import logging
 
 from dotenv import load_dotenv
 import bs4
@@ -30,24 +30,59 @@ splits = text_splitter.split_documents(docs)
 vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
 # Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
-# we need to add the streaming=True
+retriever = vectorstore.as_retriever().with_config(
+    tags=["retriever"]
+)
+# We need to add streaming=True
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, streaming=True)
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+contextualize_q_chain = (contextualize_q_prompt | llm | StrOutputParser()).with_config(
+    tags=["contextualize_q_chain"]
+)
+
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+
+def contextualized_question(input: dict):
+    if input.get("chat_history"):
+        return contextualize_q_chain
+    else:
+        return input["question"]
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-rag_chain_from_docs = (
-    RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
-    | prompt
+rag_chain = (
+    RunnablePassthrough.assign(context=contextualize_q_chain | retriever | format_docs)
+    | qa_prompt
     | llm
-    | StrOutputParser()
+).with_config(
+    tags=["main_chain"]
 )
-
-rag_chain_with_source = RunnableParallel(
-    {"context": retriever, "question": RunnablePassthrough()}
-).assign(answer=rag_chain_from_docs)
 
 app = FastAPI()
 
@@ -78,23 +113,35 @@ def serialize_aimessagechunk(chunk):
 
 async def generate_chat_events(message):
     try:
-        
-        async for event in rag_chain_with_source.astream_events(message, version="v1"):
-            
-            if event["event"] == "on_chat_model_stream":
+        async for event in rag_chain.astream_events(message, version="v1"):
+            # Only get the answer
+            sources_tags = ['seq:step:3', 'main_chain']
+            if all(value in event["tags"] for value in sources_tags) and event["event"] == "on_chat_model_stream":
                 chunk_content = serialize_aimessagechunk(event["data"]["chunk"])
                 if len(chunk_content) != 0:
                     data_dict = {"data": chunk_content}
                     data_json = json.dumps(data_dict)
                     yield f"data: {data_json}\n\n"
-            if event["event"] == "on_retriever_end":
+
+            # Get the reformulated question
+            sources_tags = ['seq:step:2', 'main_chain', 'contextualize_q_chain']
+            if all(value in event["tags"] for value in sources_tags) and event["event"] == "on_chat_model_stream":
+                chunk_content = serialize_aimessagechunk(event["data"]["chunk"])
+                if len(chunk_content) != 0:
+                    data_dict = {"reformulated": chunk_content}
+                    data_json = json.dumps(data_dict)
+                    yield f"data: {data_json}\n\n"
+                
+            # Get the context
+            sources_tags = ['main_chain', 'retriever']
+            if all(value in event["tags"] for value in sources_tags) and event["event"] == "on_retriever_end":
                 documents = event['data']['output']['documents']
-                # Créer une nouvelle liste qui va contenir les documents formatés
+                # Create a new list to contain the formatted documents
                 formatted_documents = []
-                # Parcourir chaque document dans la liste originale
+                # Iterate over each document in the original list
                 for doc in documents:
                     
-                    # Créer un nouveau dictionnaire pour chaque document avec le format requis
+                    # Create a new dictionary for each document with the required format
                     formatted_doc = {
                         'page_content': doc.page_content,
                         'metadata': {
@@ -102,24 +149,24 @@ async def generate_chat_events(message):
                         },
                         'type': 'Document'
                     }
-                    # Ajouter le document formaté à la liste finale
+                    # Add the formatted document to the final list
                     formatted_documents.append(formatted_doc)
 
-                # Créer le dictionnaire final avec la clé "context"
+                # Create the final dictionary with the key "context"
                 final_output = {'context': formatted_documents}
 
-                # Convertir le dictionnaire en chaîne JSON
+                # Convert the dictionary to a JSON string
                 data_json = json.dumps(final_output)
                 yield f"data: {data_json}\n\n"
             if event["event"] == "on_chat_model_end":
-                print("Chat model has completed its response.")
+                print("Chat model has completed one response.")
 
     except Exception as e:
         print('error'+ str(e))
 
 @app.get("/chat_stream/{message}")
 async def chat_stream_events(message: str):
-    return StreamingResponse(generate_chat_events(message), media_type="text/event-stream")
+    return StreamingResponse(generate_chat_events({"question": message, "chat_history": []}), media_type="text/event-stream")
 
 if __name__ == "__main__":
     import uvicorn
